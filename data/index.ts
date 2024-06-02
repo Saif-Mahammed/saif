ce88d3e3d795b19172af13f313c04b4296a10da5
Saif Mahammed <133027840+Saif-Mahammed@users.noreply.github.com>
2024-06-02 17:17:03 +0530

    Add files via upload

---
+++ data/index.ts
@@ -0,0 +1,165 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development and networking.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently traning a AI Model.",
+    description: "Phantom Nexus",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Movix.",
+    des: "Your personalized cinematic journey awaits, where seamless streaming and captivating storytelling converge to redefine entertainment.",
+    img: "/1.svg",
+    iconLists: ["/re.svg", "/tail.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Let Me Know.",
+    des: "Your virtual laboratory for mastering the art of web scraping, catering to both seasoned developers and coding beginners alike.",
+    img: "/2.svg",
+    iconLists: ["/next.svg", "/tail.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Phantom Media.",
+    des: "Revolutionize your online video experience with Phantom Media, offering a sleek interface, personalized recommendations, and a global community of creators.",
+    img: "/3.svg",
+    iconLists: ["/re.svg", "/tail.svg"],
+    link: "/ui.aiimg.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Saif was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Saif's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Saif is the ideal partner.",
+    name: "Hemant Yadav",
+    title: "A Member of Dignity of Nobel",
+  },
+];
+
+export const companies = [
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
